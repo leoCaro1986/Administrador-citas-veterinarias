70e51020e6c8f1e498617af492954d6205883240
Leonardo <leocv0205@gmail.com>
2021-11-21 18:42:18 -0500

    Se crea formularios, state y se capturan datos

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,27 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 
 const Formulario = () => {
+
+    //Crear usestate de cita
+    const [cita, actualizarCita] = useState ({
+        mascota: '',
+        propietario: '',
+        fecha: '',
+        hora: '',
+        sintomas: ''
+    });
+
+    //funcion que se ajecuta cada que el usuario escribe en un input
+    const actualizarState = e => {
+        actualizarCita({
+            ...cita,
+            [e.target.name]: e.target.value 
+        })
+    }
+
+    //Extraer valores
+    const{mascota, propietario, fecha, hora, sintomas } = cita;
+
     return ( 
         <Fragment>
             <h2>Crear Cita</h2>
@@ -12,14 +33,18 @@ const Formulario = () => {
                     name="mascota"
                     className="u-full-width"
                     placeholder="Nombre de la Mascota"
+                    onChange={actualizarState}
+                    value={mascota}
                 />
 
                 <label>Nombre del Dueño</label>
                 <input 
                     type="text"
-                    name="Propietario"
+                    name="propietario"
                     className="u-full-width"
                     placeholder="Nombre del dueño"
+                    onChange={actualizarState}
+                    value={propietario}
                 />
 
                 <label>Fecha</label>
@@ -27,18 +52,24 @@ const Formulario = () => {
                     type="date"
                     name="fecha"
                     className="u-full-width"
+                    onChange={actualizarState}
+                    value={fecha}
                 />
                 <label>Hora</label>
                 <input 
                     type="time"
                     name="hora"
                     className="u-full-width"
+                    onChange={actualizarState}
+                    value={hora}
                 />
 
                 <label>Sintomas</label>
                 <textarea
                     className="u-full-width"
                     name="sintomas"
+                    onChange={actualizarState}
+                    value={sintomas}
                 >
                 </textarea>
 
